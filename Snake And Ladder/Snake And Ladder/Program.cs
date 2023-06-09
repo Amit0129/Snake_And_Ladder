a64fcc691c238d7cafdfa188fd13872b14f64e6c
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-09 23:48:28 +0530

    [Amit Kumar]Added : The player rolls the Dice to get a number between 1 to 6

--- Snake And Ladder/Snake And Ladder/Program.cs
+++ Snake And Ladder/Snake And Ladder/Program.cs
@@ -7,6 +7,10 @@
             Console.WriteLine("Welcome to Snake And Ladder game!");
             int position = 0;
             Console.WriteLine("Player now in {0}th position",position);
+
+            Random random = new Random();
+            int diceRoll = random.Next(1, 7);
+            Console.WriteLine($"The value of dice after roll is : {diceRoll}");
         }
     }
 }
