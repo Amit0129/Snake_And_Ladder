b3c516d9a8861f594bb22247f26cf92b54044d30
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-12 09:36:22 +0530

    [Amit Kumar]Added : Play the game with two playear

--- Snake And Ladder/Snake And Ladder/Program.cs
+++ Snake And Ladder/Snake And Ladder/Program.cs
@@ -6,66 +6,136 @@ namespace Snake_And_Ladder
     {
         public static int No_play(int position)
         {
-            Console.WriteLine($"The Player position is : {position}");
+            Console.WriteLine("NO-PLAY");
+            //Console.WriteLine($"The Player position is : {position}");
             return position;
         }
         public static int Ladder(int position, int roll, int winPosition)
         {
+            Console.WriteLine("LADDER");
             position += roll;
             if (position > winPosition)
             {
                 position -= roll;
             }
-            Console.WriteLine("Playear position now is {0}", position);
+            //Console.WriteLine("Player position now is {0}", position);
             return position;
         }
         public static int Snake(int position, int diceRoll)
         {
+            Console.WriteLine("SNAKE");
             position -= diceRoll;
             if (position < 0)
             {
                 position = 0;
             }
+            //Console.WriteLine("Player position now is {0}", position);
+
             return position;
         }
 
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to Snake And Ladder game!");
+            Console.WriteLine("Welcome to Snake And Ladder Game");
 
-            int position = 0;
-            int winPosition = 100;
-            int diceCount = 0;
+            const int WIN_POSITION = 100;
+            int playerOnePosi = 0;
+            int playerTwoPosi = 0;
+            int diceCountPlayOne = 0;
+            int diceCountPlayTwo = 0;
 
-            Console.WriteLine("Playear position now is {0}", position);
+            bool player1 = true;
+            bool player2 = false;
+
             Random random = new Random();
-
-            while (position < 100)
+            while (playerOnePosi < WIN_POSITION && playerTwoPosi < WIN_POSITION)
             {
-                diceCount++;
 
-                int rollDice = random.Next(1, 7);
-                Console.WriteLine($"The value of dice after roll is : {rollDice}");
-
-                int checkOption = random.Next(1, 4);
-                Console.WriteLine("Check Option is {0}", checkOption);
-
-                switch (checkOption)
+                if (player1)
                 {
-                    case 1:
-                        position = No_play(position);
+                    Console.WriteLine("Player One Playing");
+                    diceCountPlayOne++;
 
-                        break;
-                    case 2:
-                        position = Ladder(position, rollDice, winPosition);
-                        break;
-                    case 3:
-                        position = Snake(position, rollDice);
-                        break;
+                    int rollDice = random.Next(1, 7);
+                    Console.WriteLine($"The value of dice after roll is : {rollDice}");
+
+                    int checkOption = random.Next(1, 4);
+                    Console.WriteLine("Check Option is {0}", checkOption);
+
+
+                    if (checkOption == 1)
+                    {
+                        playerOnePosi = No_play(playerOnePosi);
+                        Console.WriteLine("Player One position now is {0}", playerOnePosi);
+                        player1 = false;
+                        player2 = true;
+                    }
+                    else if (checkOption == 2)
+                    {
+                        playerOnePosi = Ladder(playerOnePosi, rollDice, WIN_POSITION);
+                        Console.WriteLine("Player One position now is {0}", playerOnePosi);
+                        player1 = false;
+                        player2 = true;
+                    }
+                    else
+                    { 
+                        playerOnePosi = Snake(playerOnePosi, rollDice);
+                        Console.WriteLine("Player One position now is {0}", playerOnePosi);
+                        player1 = false;
+                        player2 = true;
+
+                    }
                 }
+                else
+                {
+                    Console.WriteLine("Player Two Playing");
+                    diceCountPlayTwo++;
+
+                    int rollDice = random.Next(1, 7);
+                    Console.WriteLine($"The value of dice after roll is : {rollDice}");
+
+                    int checkOption = random.Next(1, 4);
+                    Console.WriteLine("Check Option is {0}", checkOption);
+
+
+                    if (checkOption == 1)
+                    {
+                        playerTwoPosi = No_play(playerTwoPosi);
+                        Console.WriteLine("Player Two position now is {0}", playerTwoPosi);
+
+                        player1 = true;
+                        player2 = false;
+                    }
+                    else if (checkOption == 2)
+                    {
+                        playerTwoPosi = Ladder(playerTwoPosi, rollDice, WIN_POSITION);
+                        Console.WriteLine("Player Two position now is {0}", playerTwoPosi);
+                        player1 = true;
+                        player2 = false;
+                    }
+                    else
+                    {
+                        playerTwoPosi = Snake(playerTwoPosi, rollDice);
+                        Console.WriteLine("Player Two position now is {0}", playerTwoPosi);
+                        player1 = true;
+                        player2 = false;
+
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("=====*****=====");
             }
-            Console.WriteLine($"Total number of dice played to win is : {diceCount}");
+            if (playerOnePosi == WIN_POSITION)
+            {
+                Console.WriteLine("Player One Win");
+                Console.WriteLine($"Total number of dice played by player 1 to win is : {diceCountPlayOne}");
+            }
+            if (playerTwoPosi == WIN_POSITION)
+            {
+                Console.WriteLine("Player Two Win");
+                Console.WriteLine($"Total number of dice played by player 2 to win is : {diceCountPlayTwo}");
+            }
         }
     }
 }
